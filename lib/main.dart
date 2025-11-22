e3b539511402512076f09c2fc6c338815ed1662c
Karuki <muhammadaminaziz03@gmail.com>
2025-11-22 20:39:22 +0700

    firebase_options.dart included to gitignore

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:exercise_app/pages/login_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'firebase/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
